2e3b236f8e085a3aa9e9ed668f992a66ae46b10d
Divan Visagie <visagiedivan@gmail.com>
2018-03-02 21:15:15 +0200

    remove graphite for now

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"time"
 
-	"github.com/marpaia/graphite-golang"
+	// "github.com/marpaia/graphite-golang"
 	"github.com/taiyoh/go-cm160"
 )
 
 func main() {
-	g, err := graphite.NewGraphite("localhost", 2003)
-	if err != nil {
-		panic(err)
-	}
+	// g, err := graphite.NewGraphite("localhost", 2003)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	Send := func(buf *cm160.Record) {
 		fmt.Println(buf)
@@ -25,11 +24,14 @@ func main() {
 			t = time.Date(buf.Year, time.Month(buf.Month), buf.Day, buf.Hour, buf.Minute, 0, 0, time.UTC)
 		}
 
-		m := graphite.Metric{Name: "electricity.amps", Value: strconv.FormatFloat(float64(buf.Amps), 'f', -1, 32), Timestamp: t.Unix()}
-		err := g.SendMetric(m)
-		if err != nil {
-			panic(err)
-		}
+		// m := graphite.Metric{Name: "electricity.amps", Value: strconv.FormatFloat(float64(buf.Amps), 'f', -1, 32), Timestamp: t.Unix()}
+
+		fmt.Println(buf.Watt, t)
+
+		// err := g.SendMetric(m)
+		// if err != nil {
+		// 	panic(err)
+		// }
 	}
 
 	device := cm160.Open()
